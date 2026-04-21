dbcef1f1c3067ab0c88f23cacfce4881d9779414
Srinivasan C <srinisv1245@gmail.com>
2026-04-21 14:28:47 +0530

    Add files via upload

---
+++ srini_2py.py
@@ -0,0 +1,64 @@
+'''
+#1.
+a=int(input("Enter the number(a): "))
+b=int(input("Enter the number(b): "))
+if a>b:
+    print ("b is smallest number")
+else:
+    print ("a is smallest number")
+
+#2.
+a=int(input("Enter the number(a): "))
+b=int(input("Enter the number(b): "))
+if a<b:
+    print ("b is the largest number")
+else :
+    print ("a is the largest number")
+#3.
+number=int(input("Enter the number: "))
+value= abs(number)
+print("Absolute value is:",value)
+#4.
+number=int(input("Enter the number: "))
+if number%2==0:
+  print ("it is even")
+else:
+    print("it is odd")
+
+#5.
+number=int(input("Enter the number: "))
+if number%5==0:
+    print("it is the multiple of 5")
+else:
+    print("it is not the multiple of 5")
+
+#6.
+number=int(input("Enter the number: "))
+if number%10==0:
+    print("it is the multiple of 10")
+else:
+    print("it is not the multiple of 10")
+
+#7.
+number=int(input("Enter the number: "))
+if number>=10 and number<100:
+    print("it is a two digit number")
+else :
+    print("it is not a two digit number")
+
+#8.
+number=int(input("Enter the number: "))
+if number>=100 and number<1000:
+    print("it is a three digit number")
+else:
+    print("it is not a three digit number")
+
+'''
+
+
+
+
+
+
+
+
