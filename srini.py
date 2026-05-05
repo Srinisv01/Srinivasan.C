8d3a3efada274494ece302822242d10636419ce2
Srinivasan C <srinisv1245@gmail.com>
2026-05-05 14:25:19 +0530

    Add files via upload

---
+++ srini.py
@@ -0,0 +1,48 @@
+'''
+#1.
+n = int(input("Enter the number: "))
+for i in range(1,n+1,1):
+    print(i,end=" ")
+
+#2.
+n = int(input("Enter the number: "))
+for i in range(2,n+6,2):
+    print(i,end=" ")
+#3.
+n = int(input("Enter the number: "))
+for i in range(1,n+6,2):
+    print(i,end=" ")
+#4.
+n = int(input("Enter the number: "))
+for i in range(3,n+15,3):
+    print(i,end=" ")
+#5.
+n = int(input("Enter the number: "))
+for i in range(5,n+21,5):
+    print(i,end=" ")
+#6.
+n = int(input("Enter the number: "))
+for i in range(2,n,2):
+    print(i,end=" ")
+#7.
+n = int(input("Enter the number: "))
+for i in range(1,n+1,1):
+    if i%2==0 or i%3==0:
+    print(i,end=" ")
+
+#8.
+n = int(input("Enter the number: "))
+for i in range(1,n+1,1):
+    if i%2==0 or i%7==0 or i%5:
+        print(i,end=" ")
+#9.
+n = int(input("Enter the number: "))
+for i in range(1,n+9,1):
+    if i%5==0 or i%3==0 or i%7==0:
+        print(i,end=" ")
+        '''
+for i in range(1,11,1):
+    print(i,end=" ")
+
+
+        
